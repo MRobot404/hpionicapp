e55122a02309c5a6052fbc1eeae6f8d647da2f85
MRobot404 <qomc16@gmail.com>
2022-07-12 11:00:05 -0600

    login finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  iniciarSesion = true;
+  usuarioInvalido: boolean=false;
+  nombre:string;
   constructor() {}
+  numero:any={};
+
+  ingresar(numero){
+    if(numero == 143){
+    this.iniciarSesion=false;
+    }else{
+      this.iniciarSesion=true;
+      this.usuarioInvalido=true;
+    }
+  }
+ 
+
 }
